9bed6ea0e406958f5a3f971d5ed29e4a20e97bac
TXT-57 <travonwilliams321@gmail.com>
2023-06-27 09:55:35 -0700

    added html css js

---
+++ challenges/bulb.js
@@ -0,0 +1,8 @@
+var img = document.getElementById ("lightbulb");
+    function off(){
+            img.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJoESOYsoYU8-eWwn4ppqb-15A4ObByk4ALzkg5rV8pQ&s";
+    }
+    function on(){
+            img.src="https://images.pexels.com/photos/577514/pexels-photo-577514.jpeg?cs=srgb&dl=pexels-led-supermarket-577514.jpg&fm=jpg";
+    }
+    
